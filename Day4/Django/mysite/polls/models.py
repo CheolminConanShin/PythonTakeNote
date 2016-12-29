2650392f4f5c00dc676b304816fbd9fc9dc16fb6
CheolminConanShin <scm0226@gmail.com>
2016-12-29 16:10:01 +0900

    django poll and choice models are added

--- Day4/Django/mysite/polls/models.py
+++ Day4/Django/mysite/polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
+class Poll(models.Model):
+    question = models.CharField('설문내용', max_length=200)
+    pub_date = models.DateTimeField('설문 저장 날짜')
+    def __str__(self): # 문자열을 리턴하는 메서드
+        return self.question
+    # 최근에 발행된 설문인지 여부를 리턴
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField('설문 보기', max_length=200)
+    votes = models.IntegerField()
+    def __str__(self):
+        return self.choice
